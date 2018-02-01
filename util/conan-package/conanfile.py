dfe28ab87d1d378c2c0258e2cb907a21686e39f5
danimtb <danimanzaneque@gmail.com>
2018-02-01 16:35:47 +0100

    Updates conan recipe and test_package

--- util/conan-package/conanfile.py
+++ util/conan-package/conanfile.py
@@ -3,18 +3,15 @@ from conans import ConanFile
 
 class GlmConan(ConanFile):
     name = "glm"
-    version = "master"
+    version = "0.9.8.5"
     generators = "txt"
-    url="https://github.com/g-truc/glm"
-    description="OpenGL Mathematics (GLM)"
-    license = "https://github.com/g-truc/glm/blob/manual/copying.txt"
+    url = "https://github.com/g-truc/glm"
+    description = "OpenGL Mathematics (GLM)"
+    license = "The Happy Bunny License (Modified MIT License)"
     exports_sources = ["FindGLM.cmake", os.sep.join(["..", "..", "glm*"])]
     exports = "lib_licenses/*"
-
-    def build(self):
-        self.output.info("No compilation necessary for GLM")
 
     def package(self):
         self.copy("FindGLM.cmake", ".", ".")
         self.copy("*", src="glm", dst=os.sep.join([".", "include", "glm"]))
-        self.copy("lib_licenses/license*", dst="licenses",  ignore_case=True, keep_path=False)
+        self.copy("lib_licenses/license*", dst="licenses", ignore_case=True, keep_path=False)
