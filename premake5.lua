58ce033f6e0bba750537a01b66a6c09cb35d8d12
alexpanter <alex_c007@hotmail.com>
2022-07-13 02:51:12 +0200

    Add premake file

---
+++ premake5.lua
@@ -0,0 +1,17 @@
+project "glm"
+
+  kind "StaticLib"
+  language "C++"
+
+  targetdir ("../../bin/" .. outputdir .. "/%{prj.name}")
+  objdir ("../../obj/" .. outputdir .. "/%{prj.name}")
+
+  files
+  {
+	 "glm/detail/glm.cpp"
+  }
+
+  defines
+  {
+	 "GLM_ENABLE_EXPERIMENTAL"
+  }
