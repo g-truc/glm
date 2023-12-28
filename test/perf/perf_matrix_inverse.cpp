934a7a7bc98192b70372e4d4ed6f0935e8819b9b
christophe <christophe@g-truc.net>
2023-12-28 10:06:38 +0100

    Disable perf test by default

--- test/perf/perf_matrix_inverse.cpp
+++ test/perf/perf_matrix_inverse.cpp
@@ -4,6 +4,7 @@
 #include <glm/ext/matrix_double4x4.hpp>
 #include <glm/ext/matrix_relational.hpp>
 #include <glm/ext/vector_float4.hpp>
+
 #if GLM_CONFIG_SIMD == GLM_ENABLE
 #include <glm/gtc/type_aligned.hpp>
 #include <vector>
