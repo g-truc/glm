44c4d326b785381180791a9f6b8155e5ab5570a8
ZXShady <153229951+ZXShady@users.noreply.github.com>
2025-03-07 20:38:27 +0000

    Add simple test for `vec_swizzle`

--- test/gtx/gtx_vec_swizzle.cpp
+++ test/gtx/gtx_vec_swizzle.cpp
@@ -6,6 +6,27 @@ int main()
 	int Error = 0;
 
 
+
+
+
+	{
+		glm::vec2 const v(1, 2);
+		Error += xx(v).x != xx(v).y;
+		Error += yy(v).x != yy(v).y;
+	}
+
+	{
+		glm::vec3 const v(1, 2, 3);
+		Error += zyx(v) != glm::vec3(3, 2, 1);
+	}
+
+	{
+		glm::vec4 const v(1, 2, 3, 4);
+		Error += wwww(v) != glm::vec4(4);
+		Error += wzyx(v) != glm::vec4(4,3,2,1);
+
+	}
+
 	return Error;
 }
 
